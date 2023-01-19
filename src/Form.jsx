21ef594b372257b3cb07cebf7ed37a8b81fbe481
cathy <cathy@bubulazi.com>
2023-01-19 18:34:28 +0000

    Dando estilo, textarea y fieldset

--- src/Form.jsx
+++ src/Form.jsx
@@ -3,17 +3,22 @@ import React from 'react'
 import './Form.css'
 
 function Form() {
-  const [formData, setFormData] = React.useState({
-    nombre: '',
-    apellido: ''
-  })
+  const [formData, setFormData] = React.useState(
+    {
+          nombre: '',
+          apellido: '',
+          email: '',
+          comentarios: '',
+          esAmigo: true,
+          empleo: ''
+    })
   console.log(formData)
 
   function manejarCambio(e) {
-    const {name,value} = e.target.value
+    const {name,value,type, checked} = e.target
     setFormData(preFormData => ({
       ...preFormData, 
-      [name]: value
+      [name]: type === 'checkbox' ? checked :  value
 
     }))
 
@@ -25,13 +30,73 @@ function Form() {
           placeholder='Nombre'
           onChange={manejarCambio}
           name='nombre'
+          value={formData.nombre}
       />
            <input 
           type="text" 
           placeholder='Apellido'
           onChange={manejarCambio}
           name='apellido'
+          value={formData.apellido}
       />
+          <input 
+          type="text" 
+          placeholder='Email'
+          onChange={manejarCambio}
+          name='email'
+          value={formData.email}
+      />
+      <textarea 
+          value={formData.comentarios}
+          placeholder="Comentarios"
+          onChange={manejarCambio}
+          name='comentarios'
+      />
+        <input 
+          type="checkbox" 
+          id="esAmigo" 
+          checked={formData.esAmigo}
+          onChange={manejarCambio}
+          name="esAmigo"
+          />
+          <label htmlFor="esAmigo">¿Eres amistosos?</label>
+          <br />
+          <br />
+
+          <fieldset>
+                <legend>Estado actual de empleo</legend>
+                
+                <input 
+                    type="radio"
+                    id="desempleados"
+                    name='empleo'
+                    value={desempleados}
+                    checked={formData.empleo === 'desempleados'}
+                />
+                <label htmlFor="desempleados">Desempleados</label>
+                <br />
+                
+                <input 
+                    type="radio"
+                    id="tiempo-parcial"
+                    name='empleo'
+                    value={tiempo-parcial}
+                    checked={formData.empleo === 'tiempo-parcial'}
+                />
+                <label htmlFor="tiempo-parcial">Tiempo parcial</label>
+                <br />
+                
+                <input 
+                    type="radio"
+                    id="tiempo-completo"
+                    name='empleo'
+                    value={tiempo-completo}
+                    checked={formData.empleo === 'tiempo-completo'}
+                />
+                <label htmlFor="tiempo-completo">Tiempo-completo</label>
+                <br />
+                
+            </fieldset>
 
     </form>
   )
